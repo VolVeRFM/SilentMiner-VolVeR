531460f337c7b8770c8771555c755658c63a8481
VolVeR <52369870+VolVeRFM@users.noreply.github.com>
2023-02-25 04:02:23 +0500

    Update Program.cs

--- VolVeRBuilder/Program.cs
+++ VolVeRBuilder/Program.cs
@@ -19,7 +19,7 @@ namespace VolVeRBuilder
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
+            Application.Run(new Form1());
            
 
 
